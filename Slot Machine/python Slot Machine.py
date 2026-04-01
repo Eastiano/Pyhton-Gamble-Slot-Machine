4707c52018c956c21d0457072fa21a416a2c6982
Daniel Oluwaseyi <danieloluwaseyi11@gmail.com>
2026-04-01 20:42:15 +0100

    Add files via upload

---
+++ Slot Machine/python Slot Machine.py
@@ -0,0 +1,84 @@
+# python Slot Machine
+
+import time
+import random
+
+def spin_row():
+    symbols = ['🍋', '🍒', '🍉', '⭐', '🔔']
+
+    return [random.choice(symbols) for _ in range(3)]
+
+def print_row(row):
+    print("************")
+    print(" | ".join(row)) # use join method to take list and join each element with space in between
+    print("************")
+
+def get_payout(row, bet):
+    if row[0] == row[1] == row[2]:
+        if row[0] == '⭐':
+            return bet * 3
+        elif row[0] == '🔔':
+            return bet * 4
+        elif row[0] == '🍉':
+            return bet * 5
+        elif row[0] == '🍒':
+            return bet * 10
+        elif row[0] == '🍋':
+            return bet * 20
+    return 0
+
+def main():
+    balance = 100
+
+    print("**************************")
+    print("Welcome to Daniel Slot's!")
+    print("**************************")
+    print("Available Symbols: 🍋🍒🍉⭐🔔")
+    print("***************************")
+
+    while balance > 0:
+        print(f"Current Balance: ${balance}")
+
+        bet = input("Place your bet amount: ")
+
+        if not bet.isdigit():
+            print("Please enter a vadlid number")
+            continue
+
+        bet = int(bet)
+
+        if bet > balance:
+            print("Insufficient balance. Please enter a lower bet.")
+            continue
+
+        if bet <= 0:
+            print("Bet must be greater than 0.")
+            continue
+
+        balance -= bet
+
+        row = spin_row()
+        print("Spinning...\n")
+        time.sleep(1) # Adds suspense for the user
+        print_row(row)
+        
+        payout = get_payout(row, bet)
+
+        if payout > 0:
+            print(f"You won ${payout}!")
+        else:
+            print("You lost!")
+
+        balance += payout
+
+        play_again = input("Do you want to play again? (Y/N): ").upper()
+
+        if play_again != 'Y':
+            break
+
+    print("********************************************************")
+    print(f"You are leaving with ${balance}. Thanks for playing!")
+    print("********************************************************")
+
+if __name__ == "__main__":
+    main()
